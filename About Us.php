834479134de574208f56830df59efa746c093e2a
raulinc <rahulrawatwork@gmail.com>
2018-03-26 00:56:34 +0530

    About

---
+++ About Us.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>About</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav class="navbar navbar-default bg-info">
+            <div class="container ">
+               
+                    <a class="nav-item nav-link active text-white" href="index.php">HOME</a>
+                    <a class="nav-item nav-link text-white" data-toggle="modal" data-target="#exampleModal">Contact Us</a>       
+            </div>
+        </nav>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+        <div class="jumbotron jumbotron-fluid bg-info text-white">
+           <div class="container text-center">
+            <h1 class="display-1">Happy Us </h1> 
+            <p class="lead">The best quality like Ever</p>
+           </div>
+        </div>
+        <div class="container ">
+             <h2 class="display-5 text-center">Meet the ShopKeepers</h2>
+        
+       
+        <nav class="nav justify-content-center nav-pills py-5 ">
+            <a class="nav-link active" href="#pan" data-toggle="tab" >Bhaya 1</a>
+            <a class="nav-link" href="#baniya"  data-toggle="tab" >Bhaya 2</a>
+            <a class="nav-link" href="#sharma"  data-toggle="tab">Bhaya 3</a>
+        </nav>
+        <div class="tab-content">
+            <div class="tab-pane active" id="pan">
+                <h3>Pandit g</h3>
+                <p>pandit g bae badde shatir ho toh kaa hua asda asd asd ad thbf uktjh teetbw q qe eq qeqe qeqe qe qeqewoo b ka has kias asdi asdasos ...challe tehh lank lene</p>
+            </div>
+            <div class="tab-pane" id="baniya">
+                <h3>baniyee g</h3>
+                <p>passadas asd fast g bae badde shatir ...challe asdfd as;das d ad;a ajab;ae dasdadstehh lank lene</p>
+            </div>
+            <div class="tab-pane">
+                <h3>sharma g</h3>
+                <p>pandit aasasd sdadsg bae badde shatir sdfadf...challeasdfaslja adskjas asldjbasd asdasdblas dasldajsh d tehh lank lene</p>
+            </div>
+        </div>
+         </div>
+        <div class="container py-5">
+            <div class="row text-center justify-content">
+                <div class="col-sm-6 col-md-4">
+                    <div class="card bg-info ">
+                        <img src="img/224.png"  class="img-thumbnail rounded-circle" >
+                        <div class="card-block">
+                            <h3 class="card-title ">Bat bhaiya</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4" >
+                    <div class="card bg-info">
+                        <img src="img/62.jpg"  class="img-thumbnail rounded-circle">
+                        <div class="card-block">
+                            <h3 class="card-title">Captian Pandit</h3>
+                        </div>
+                    </div>
+                </div>
+               
+                <div class="col-sm-6 col-md-4">
+                    <div class="card bg-info ">
+                        <img src="img/er.png" class="img-thumbnail rounded-circle">
+                        <div class="card-block">
+                            <h3 class="card-title">Super Baniya</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      <footer>
+      <div class="container-fluid bg-info">
+          <center>
+              <h5>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</h5>	
+          </center>
+      </div>
+  </footer> 
+        <?php
+        // put your code here
+        ?>
+    </body>  
+       
+</html>
