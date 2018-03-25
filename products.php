c0ab02d0603195d0617a2ccd1da7ee4114eebec4
raulinc <rahulrawatwork@gmail.com>
2018-03-26 00:57:36 +0530

    re build

--- products.php
+++ products.php
@@ -1,74 +1,64 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!doctype HTML>
 <html>
-<head>
-    <title>Products</title>
-    <meta charset="UTF-8">
-
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo|Chenlax|CrimsonText"/>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-        <!--jQuery library--> 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!--Latest compiled and minified JavaScript--> 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-        <link type="text/css" href="css/style.css" rel="stylesheet"/>
+    <head>
+        <title>
+            Welcome | Happy Store
+        </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                <link type="text/css" href="css/style.css" rel="stylesheet"/>
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-</head>
-<body>
-	<header class="container-fluid " bg-color:blue>
-            <img src="img/intro-bg_1.jpg" alt="Me">
-                <div class="w3-display-middle w3-padding-large w3-border w3-wide w3-text-light-grey w3-center">
-                      <h1 class="w3-hide-medium w3-hide-small w3-xxxlarge">JANE DOE</h1>
-                      <h5 class="w3-hide-large" style="white-space:nowrap">JANE DOE</h5>
-                      <h3 class="w3-hide-medium w3-hide-small">PHOTOGRAPHER</h3>
-                </div>
-  
- 
-  
-</header>	<!-- Header -->
-       <nav class="navbar navbar-default">
-	
+    </head>
+    <body>
+        <nav class="navbar bg-info navbar-default">
             <div class="container">
-                <div class="navbar-header ">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>                        
-                    </button>
-			<a class="navbar-brand" href="index.php">Happy Store</a>
-					
-                </div>
-				
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-			<li><a href="signup.html"><span class="glyphicon glyphicon-plus-sign"></span> More</a></li>
-                        <li><a href="login.html"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
-                        <li><a href="signup.html"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-                    </ul>
-					
-		</div>
-				
+                <h2 class="display-4 text-white">Happy Store</h2>
+                <div class="row float-right">
+                    <a class="nav-item nav-link text-white" data-toggle="modal" data-target="#exampleModal">Login</a>
+                    <a class="nav-item nav-link text-white " href="cart.php">Cart</a> 
+                    <a class="nav-item nav-link text-white " href="About Us.php">About Us</a>  
+                </div>   
             </div>
-	</nav>
-          
-            
-           
-      
-</body>
-<footer>
-      <div class="container">
-          <center>
-              <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>	
-          </center>
+                        
+        </nav>
+        <div class="container">
+            <div class="row">
+           <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+             <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <form class='form-group'>
+              <input class="form-control" type='text' name='name' placeholder="User Id"/>
+              <input class="form-control" type='password' name='pass' placeholder="Password"/>
+              <button type="submit" class="btn btn-primary">Login</button>
+            </form>
       </div>
- </footer>
+        <div class="modal-footer">
+            <p>Don't have an account</p>
+            <button type="button" class="btn btn-primary"><a class="text-white" href="signup.php">SignUp</a></button>
+       
+      </div>
+    </div>
+  </div>
+</div>
+            </div>
+        </div>
+                 
+        <div class="container-fluid bg-info fixed-bottom">
+            <p class="text-center text-white">Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>
+        </div>       
+        
+    </body>
+    
 </html>
-
-
