481a9d430eab80643c7b5a424dba6a1d69040f04
raulinc <rahulrawatwork@gmail.com>
2018-03-27 10:54:34 +0530

    footer added

--- About Us.php
+++ About Us.php
@@ -7,6 +7,8 @@ and open the template in the editor.
 <html>
     <head>
         <title>About</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -19,8 +21,8 @@ and open the template in the editor.
         <nav class="navbar navbar-default bg-info">
             <div class="container ">
                
-                    <a class="nav-item nav-link active text-white" href="index.php">HOME</a>
-                    <a class="nav-item nav-link text-white" data-toggle="modal" data-target="#exampleModal">Contact Us</a>       
+                    <a class="nav-item nav-link text-white lead" href="index.php">HOME</a>
+                    <a class="nav-item nav-link text-white lead" data-toggle="modal" data-target="#exampleModal">Ask Us</a>       
             </div>
         </nav>
 
@@ -29,30 +31,30 @@ and open the template in the editor.
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Ask Us</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+          <form>
+              <input class="form-control form-group"type="text" name="cnt" placeholder="E-mail/Contact No">
+              <input class="form-control form-group" type="text-field" name="content" place="Query">
+          </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+           <button type="button" class="btn btn-primary">Submit</button>
       </div>
     </div>
   </div>
 </div>
 
 
-
-
-
-        <div class="jumbotron jumbotron-fluid bg-info text-white">
+<div class="jumbotron jumbotron-fluid bg-info text-white">
            <div class="container text-center">
             <h1 class="display-1">Happy Us </h1> 
-            <p class="lead">The best quality like Ever</p>
+            <p class="lead py-3">The best quality like Ever
+             Avengers are a fictional team of superheroes appearing in American comic books published by Marvel Comics. The team made its debut in The Avengers #1 (cover-dated Sept. 1963), created by writer-editor Stan Lee and artist/co-plotter Jack Kirby. The Avengers is Lee and Kirby's renovation of a previous superhero team, All-Winners Squad, that appeared in comic books series published Marvel Comics' predecessor Timely Comics.</p>
            </div>
         </div>
         <div class="container ">
@@ -60,14 +62,41 @@ and open the template in the editor.
         
        
         <nav class="nav justify-content-center nav-pills py-5 ">
-            <a class="nav-link active" href="#pan" data-toggle="tab" >Bhaya 1</a>
+            <a class="nav-link active" href="#pan" data-toggle="tab" >Bat Bhaya</a>
             <a class="nav-link" href="#baniya"  data-toggle="tab" >Bhaya 2</a>
             <a class="nav-link" href="#sharma"  data-toggle="tab">Bhaya 3</a>
         </nav>
         <div class="tab-content">
             <div class="tab-pane active" id="pan">
-                <h3>Pandit g</h3>
-                <p>pandit g bae badde shatir ho toh kaa hua asda asd asd ad thbf uktjh teetbw q qe eq qeqe qeqe qe qeqewoo b ka has kias asdi asdasos ...challe tehh lank lene</p>
+                <h3>Bat Bhaiya</h3>
+                <p>Batman Batman DC Comics.png Art by Mikel Janín Publication information Publisher DC Comics First appearance	Detective Comics #27cover date May 1939 / release date March 1939) Created by	
+Bob Kane
+Bill Finger[1]
+In-story information
+Alter ego	Bruce Wayne
+Team affiliations	
+Batman Family
+Justice League
+Outsiders
+Batmen of All Nations
+Batman Incorporated
+Partnerships	
+Robin (various)
+James Gordon
+Superman
+Wonder Woman
+Catwoman
+Batgirl (various)
+Notable aliases	
+Matches Malone[2]
+Lefty Knox[3]
+Abilities	
+Genius-level intellect
+Peak human physical condition
+Skilled martial artist and hand-to-hand combatant
+Expert detective
+Utilizes high-tech equipment and weapons
+Batman is a superhero by DC Comics.</p>
             </div>
             <div class="tab-pane" id="baniya">
                 <h3>baniyee g</h3>
@@ -81,43 +110,81 @@ and open the template in the editor.
          </div>
         <div class="container py-5">
             <div class="row text-center justify-content">
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-4 col-xs-6 px4">
                     <div class="card bg-info ">
-                        <img src="img/224.png"  class="img-thumbnail rounded-circle" >
+                        <img src="img/224.png" class="img-thumbnail rounded-circle" >
                         <div class="card-block">
                             <h3 class="card-title ">Bat bhaiya</h3>
+                            <p class="lead">batbhaiya@hero.co</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-4" >
+                <div class="col-sm-6 col-md-4 col-xs-6 px4" >
                     <div class="card bg-info">
                         <img src="img/62.jpg"  class="img-thumbnail rounded-circle">
                         <div class="card-block">
                             <h3 class="card-title">Captian Pandit</h3>
+                            <p class="lead">captainpandit@hero.co</p>
                         </div>
                     </div>
                 </div>
                
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-4 col-xs-6 px4">
                     <div class="card bg-info ">
                         <img src="img/er.png" class="img-thumbnail rounded-circle">
                         <div class="card-block">
                             <h3 class="card-title">Super Baniya</h3>
+                            <p class="lead">superbaniya@hero.co</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-      <footer>
-      <div class="container-fluid bg-info">
-          <center>
-              <h5>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</h5>	
-          </center>
-      </div>
-  </footer> 
-        <?php
-        // put your code here
-        ?>
+      <div class="footer">
+        
+        
+            <center>
+                
+                <p class="lead text-white">Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>
+            <table class="table table-condensed text-white lead">
+                <thead>
+                    <th>
+                        Information
+                    </th>
+                    <th>
+                        My Account
+                    </th>
+                    <th>
+                        Contact Us
+                    </th>
+                </thead>
+                <tr>
+                    <td>
+                        About us
+                    </td>
+                    <td>
+                        Login
+                    </td>
+                    <td>
+                        +91974780087
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Contact up
+                    </td>
+                    <td>
+                        Signup
+                    </td>
+                    <td>
+                       
+                    </td>
+                </tr>
+            </table>
+   
+            </center>
+            
+    </div>
     </body>  
        
 </html>
