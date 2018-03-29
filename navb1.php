8a8c95391ef38242a18e753881ff8eb61c426b87
raulinc <rahulrawatwork@gmail.com>
2018-03-29 18:31:28 +0530

    inclues1

---
+++ navb1.php
@@ -0,0 +1,17 @@
+
+<nav class="navbar bg-info navbar-default">
+            <div class="container">
+              
+                <div class="row float-right">
+                    <a class="nav-item nav-link text-white" href="index.php">Home</a>
+                    <a class="nav-item nav-link text-white" data-toggle="modal" data-target="#exampleModal">Login</a>
+                    <a class="nav-item nav-link text-white " href="signup.php">Cart</a> 
+                    <a class="nav-item nav-link text-white " href="About Us.php">About Us</a>  
+                    
+                </div>
+               </div>
+          
+                        
+        </nav>
+   
+           
